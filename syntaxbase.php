b394b5189c7533d6c094f777d3cbc313b0fa8595
Andreas Gohr <andi@splitbrain.org>
2008-01-26 17:17:01 +0100

    make sure infoutils.php is available
    darcs-hash:20080126161701-7ad00-1dcee17f88cc4d6d22709fa20d60a349593d0723.gz

--- syntaxbase.php
+++ syntaxbase.php
@@ -8,6 +8,7 @@ if(!defined('DOKU_INC')) die();
 
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'syntax.php');
+require_once(DOKU_INC.'inc/infoutils.php');
 
 
 /**
