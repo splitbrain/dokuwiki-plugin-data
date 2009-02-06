8f3b9db05284ccd35366af567ca342ecf21e9131
Andreas Gohr <andi@splitbrain.org>
2009-02-06 21:33:34 +0100

    fixed URL in getInfo
    Ignore-this: 898aa65b218e9169ad1233038fcf4724
    
    darcs-hash:20090206203334-7ad00-f2734583f321e71219c88693a8f5dd4006091a50.gz

--- helper.php
+++ helper.php
@@ -39,10 +39,10 @@ class helper_plugin_data extends DokuWiki_Plugin {
         return array(
             'author' => 'Andreas Gohr',
             'email'  => 'andi@splitbrain.org',
-            'date'   => '2009-01-05',
+            'date'   => '2009-02-06',
             'name'   => 'Structured Data Plugin',
             'desc'   => 'Add and query structured data in your wiki',
-            'url'    => 'http://wiki.splitbrain.org/plugins:data',
+            'url'    => 'http://www.dokuwiki.org/plugin:data',
         );
     }
 
