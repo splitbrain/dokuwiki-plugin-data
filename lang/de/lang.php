1e1e56a1d9bbfe54da0c66c9c8a977f66c588fab
Adrian Lang <lang@cosmocode.de>
2010-02-23 12:00:00 +0100

    Add german translation

---
+++ lang/de/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['sort'] = 'Nach dieser Spalte sortieren';
+$lang['next'] = 'Nächste Seite';
+$lang['prev'] = 'Vorherige Seite';
+
+$lang['tagfilter'] = 'Seiten mit ‚%s‘ anzeigen';
+$lang['related']   = 'Ähnliche Seiten';
