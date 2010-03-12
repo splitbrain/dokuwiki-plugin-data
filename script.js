80b5814c7b61d63ae6894a07d160527476187b11
Adrian Lang <lang@cosmocode.de>
2010-03-12 12:24:33 +0100

    Correctly handle multi alias fields

--- script.js
+++ script.js
@@ -30,11 +30,15 @@ addInitEvent(function () {
         return li;
     }
 
-    var classes = { 'data_type_page': false, 'data_type_pages': true };
+    var classes = {'data_type_page' : [false, /data_type_(\w+) data_type_page/],
+                   'data_type_pages': [true, /data_type_(\w+)s data_type_pages/] };
     for (var c_class in classes) {
         var pickers = getElementsByClass(c_class, document, 'label');
         for (var i = 0 ; i < pickers.length ; ++i) {
-            AutoCompletion(pickers[i].lastChild, 'data_page_' + pickers[i].className.match(/data_type_(\w+) data_type_pages?/)[1], classes[c_class], prepare_li);
+            AutoCompletion(pickers[i].lastChild,
+                           'data_page_' + pickers[i].className.match(classes[c_class][1])[1],
+                           classes[c_class][0],
+                           prepare_li);
         }
     }
 });
