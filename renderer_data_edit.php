31cdb08599a555f3b62e5fe7d65ee8f823cd147f
Adrian Lang <lang@cosmocode.de>
2010-03-18 12:30:47 +0100

    Add require renderer.php

--- renderer_data_edit.php
+++ renderer_data_edit.php
@@ -6,6 +6,8 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  */
 
+require_once DOKU_INC . 'inc/parser/renderer.php';
+
 class Doku_Renderer_plugin_data_edit extends Doku_Renderer {
     function getFormat(){
         return 'plugin_data_edit';
