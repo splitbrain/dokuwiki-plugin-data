029389dd827e4f106206c26e3c004bba6fc3ccc3
Adrian Lang <lang@cosmocode.de>
2010-04-28 14:47:47 +0200

    Use addAutoCompletion, not AutoCompletion

--- script.js
+++ script.js
@@ -21,24 +21,23 @@ addInitEvent(function () {
  * @author Adrian Lang <lang@cosmocode.de>
  */
 addInitEvent(function () {
-    if (typeof AutoCompletion === 'undefined') return;
+    if (typeof addAutoCompletion !== 'function') return;
 
-    var alias_AutoCompletion = AutoCompletion;
-    alias_AutoCompletion.prototype.prepareLi = function (li, value) {
+    function prepareLi(li, value) {
             var name = value[0];
             li.innerHTML = '<a href="#">' + value[1] + ' (' + name + ')' + '</a>';
             li.id = 'data__' + name.replace(/\W/g, '_');
             li._value = name;
     };
-
     var classes = {'data_type_page' : [false, /data_type_(\w+) data_type_page/],
                    'data_type_pages': [true, /data_type_(\w+)s data_type_pages/] };
     for (var c_class in classes) {
         var pickers = getElementsByClass(c_class, document, 'label');
         for (var i = 0 ; i < pickers.length ; ++i) {
-            new alias_AutoCompletion(pickers[i].lastChild,
+            addAutoCompletion(pickers[i].lastChild,
                            'data_page_' + pickers[i].className.match(classes[c_class][1])[1],
-                           classes[c_class][0]);
+                           classes[c_class][0],
+                           prepareLi);
         }
     }
 });
