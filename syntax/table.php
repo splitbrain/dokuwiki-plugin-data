4b55ed055582eaf8a5dda951152060baa4c2854c
Adrian Lang <lang@cosmocode.de>
2010-06-15 17:30:54 +0200

    No secedit plz

--- syntax/table.php
+++ syntax/table.php
@@ -238,7 +238,7 @@ class syntax_plugin_data_table extends DokuWiki_Syntax_Plugin {
             $R->cdata($this->getLang('none'));
             $R->tablecell_close();
             $R->tablerow_close();
-            $R->table_close();
+            $R->doc .= '</table>';
             return true;
         }
 
