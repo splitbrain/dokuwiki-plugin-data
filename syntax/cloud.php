84701025f2898058fe8c2deeac08f41d3d67c30f
Klap-in <klapinklapin@gmail.com>
2012-06-22 21:28:10 +0200

    refer correct table in counts query of cloud

--- syntax/cloud.php
+++ syntax/cloud.php
@@ -80,7 +80,7 @@ class syntax_plugin_data_cloud extends syntax_plugin_data_table {
                     // filter by hidden column?
                     if(!$tables[$col]){
                         $tables[$col] = 'T'.(++$cnt);
-                        $from  .= ' LEFT JOIN data AS '.$tables[$col].' ON '.$tables[$col].'.pid = pages.pid';
+                        $from  .= ' LEFT JOIN data AS '.$tables[$col].' ON '.$tables[$col].'.pid = data.pid';
                         $from  .= ' AND '.$tables[$col].".key = ".$sqlite->quote_string($col);
                     }
 
