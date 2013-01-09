360050446c93c113bcfd9f482d1a6a63f94c6fac
Dominik Eckelmann <eckelmann@cosmocode.de>
2013-01-09 17:02:37 +0100

    added test for data entry syntax showData method

--- _test/syntax_plugin_data_entry.test.php
+++ _test/syntax_plugin_data_entry.test.php
@@ -1,25 +1,31 @@
 <?php
 
+require_once DOKU_INC . 'inc/parser/xhtml.php';
+
 class syntax_plugin_data_entry_test extends DokuWikiTest {
 
     protected $pluginsEnabled = array('data', 'sqlite');
 
+    private $exampleEntry;
+
+    function __construct() {
+        $this->exampleEntry = "---- dataentry projects ----\n"
+            . "type            : web development\n"
+            . "volume          : 1 Mrd    # how much do they pay?\n"
+            . "employees       : Joe, Jane, Jim\n"
+            . "customer_page   : customers:microsoft\n"
+            . "deadline_dt     : 2009-08-17\n"
+            . "server_pages    : servers:devel01, extern:microsoft\n"
+            . "website_url     : http://www.microsoft.com\n"
+            . "task_tags       : programming, coding, design, html\n"
+            . "----\n";
+    }
+
     function testHandle() {
         $plugin = new syntax_plugin_data_entry();
 
-        $entry = "---- dataentry projects ----\n"
-               . "type            : web development\n"
-               . "volume          : 1 Mrd    # how much do they pay?\n"
-               . "employees       : Joe, Jane, Jim\n"
-               . "customer_page   : customers:microsoft\n"
-               . "deadline_dt     : 2009-08-17\n"
-               . "server_pages    : servers:devel01, extern:microsoft\n"
-               . "website_url     : http://www.microsoft.com\n"
-               . "task_tags       : programming, coding, design, html\n"
-               . "----\n";
-
         $null = null;
-        $result = $plugin->handle($entry, 0, 10, $null);
+        $result = $plugin->handle($this->exampleEntry, 0, 10, $null);
 
         $this->assertEquals(10,         $result['pos'],     'Position has changed');
         $this->assertEquals(366,        $result['len'],     'wrong entry length');
@@ -78,7 +84,6 @@ class syntax_plugin_data_entry_test extends DokuWikiTest {
         $this->assertEquals($cols, $result['cols'], 'Cols array corrupted');
     }
 
-
     protected function createColumnEntry($name, $multi, $key, $title, $type) {
         return array(
             'colname' => $name,
@@ -87,6 +92,35 @@ class syntax_plugin_data_entry_test extends DokuWikiTest {
             'title' => $title,
             'type' => $type
         );
+    }
+
+    function testShowData() {
+        $xhtml = new Doku_Renderer_xhtml();
+        $plugin = new syntax_plugin_data_entry();
+
+        $null = null;
+        $result = $plugin->handle($this->exampleEntry, 0, 10, $null);
+
+        $plugin->_showData($result, $xhtml);
+        $doc = phpQuery::newDocument($xhtml->doc);
+
+        $this->assertEquals(1, pq('div.inline.dataplugin_entry.projects', $doc)->length);
+        $this->assertEquals(1, pq('dl dt.type')->length);
+        $this->assertEquals(1, pq('dl dd.type')->length);
+        $this->assertEquals(1, pq('dl dt.volume')->length);
+        $this->assertEquals(1, pq('dl dd.volume')->length);
+        $this->assertEquals(1, pq('dl dt.employee')->length);
+        $this->assertEquals(3, pq('dl dd.employee')->length);
+        $this->assertEquals(1, pq('dl dt.customer')->length);
+        $this->assertEquals(1, pq('dl dd.customer')->length);
+        $this->assertEquals(1, pq('dl dt.deadline')->length);
+        $this->assertEquals(1, pq('dl dd.deadline')->length);
+        $this->assertEquals(1, pq('dl dt.server')->length);
+        $this->assertEquals(2, pq('dl dd.server')->length);
+        $this->assertEquals(1, pq('dl dt.website')->length);
+        $this->assertEquals(1, pq('dl dd.website')->length);
+        $this->assertEquals(1, pq('dl dt.task')->length);
+        $this->assertEquals(4, pq('dl dd.task')->length);
     }
 }
 
