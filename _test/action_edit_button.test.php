c48e0a9579a2b278bb9b74a9ab18a00561aa3bba
Dominik Eckelmann <eckelmann@cosmocode.de>
2013-01-16 16:08:40 +0100

    added test for HTML_SECEDIT_BUTTON event handler

---
+++ _test/action_edit_button.test.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class data_action_plugin_edit_button_test extends DokuWikiTest {
+
+    protected $pluginsEnabled = array('data', 'sqlite');
+
+    function testSetName() {
+        $action = new action_plugin_data();
+        $data = array(
+            'target' => 'plugin_data'
+        );
+        $event = new Doku_Event('', $data);
+        $action->_editbutton($event, null);
+
+        $this->assertTrue(isset($data['name']));
+    }
+
+    function testWrongTarget() {
+        $action = new action_plugin_data();
+        $data = array(
+            'target' => 'default target'
+        );
+        $action->_editbutton($data, null);
+
+        $this->assertFalse(isset($data['name']));
+    }
+
+}
