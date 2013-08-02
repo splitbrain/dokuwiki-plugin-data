8d97993c630310d4c703d4de35f458e766626fea
Andreas Gohr <andi@splitbrain.org>
2013-08-03 00:00:09 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,33 +1,32 @@
 <?php
-$lang['menu_alias'] = 'Data Plugin: Feld-Aliasse';
-$lang['menu_clean'] = 'Data Plugin: Datenbank aufräumen';
 
-$lang['pages_del']    = '%d Einträge entfernt.';
-$lang['submit_clean'] = 'Datenbank aufräumen!';
-
-$lang['sort'] = 'Nach dieser Spalte sortieren';
-$lang['next'] = 'Nächste Seite';
-$lang['prev'] = 'Vorherige Seite';
-
-$lang['tagfilter'] = 'Seiten mit ‚%s‘ anzeigen';
-$lang['related']   = 'Ähnliche Seiten';
-
-$lang['page']      = 'Seite';
-$lang['title']     = 'Seitenname';
-$lang['class']     = 'Seitenklasse';
-
-$lang['name']      = 'Typalias';
-$lang['type']      = 'Originaltyp';
-$lang['prefix']    = 'Präfix';
-$lang['postfix']   = 'Suffix';
-$lang['enum']      = 'Gültige Werte';
-$lang['submit']    = 'Aliasse speichern';
-
-$lang['dataentry'] = 'Data-Entry';
-
-$lang['title']     = 'Titel';
-$lang['type']      = 'Typ';
-$lang['multi']     = 'Multi?';
-$lang['value']     = 'Wert';
-
-$lang['none']      = 'Nichts gefunden';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Andreas Gohr <andi@splitbrain.org>
+ */
+$lang['menu_alias']            = 'Data Plugin: Feld-Aliasse';
+$lang['menu_clean']            = 'Data Plugin: Datenbank aufräumen';
+$lang['pages_del']             = '%d Einträge entfernt.';
+$lang['submit_clean']          = 'Datenbank aufräumen!';
+$lang['sort']                  = 'Nach dieser Spalte sortieren';
+$lang['next']                  = 'Nächste Seite';
+$lang['prev']                  = 'Vorherige Seite';
+$lang['tagfilter']             = 'Seiten mit ‚%s‘ anzeigen';
+$lang['related']               = 'Ähnliche Seiten';
+$lang['page']                  = 'Seite';
+$lang['title']                 = 'Titel';
+$lang['class']                 = 'Seitenklasse';
+$lang['lastmod']               = 'Letztere Änderung';
+$lang['name']                  = 'Typalias';
+$lang['type']                  = 'Typ';
+$lang['prefix']                = 'Präfix';
+$lang['postfix']               = 'Suffix';
+$lang['enum']                  = 'Gültige Werte';
+$lang['submit']                = 'Aliasse speichern';
+$lang['dataentry']             = 'Data-Entry';
+$lang['multi']                 = 'Multi?';
+$lang['value']                 = 'Wert';
+$lang['none']                  = 'Nichts gefunden';
+$lang['tablefilteredby']       = 'Gefiltert nach %s';
+$lang['tableresetfilter']      = 'Zeige alle (Filter/Sortierung entfernen)';
