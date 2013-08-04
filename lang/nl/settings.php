10d59a8089d2aa472e72d8b99882de94c57d66da
mprins <mprins@users.sf.net>
2013-08-04 10:45:10 +0200

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -1,3 +1,11 @@
 <?php
 
-$lang['edit_content_only'] = 'Bewerk alleen de inhoud van een datablok, niet de structuur';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author mprins <mprins@users.sf.net>
+ */
+$lang['edit_content_only']     = 'Bewerk alleen de inhoud van een datablok, niet de structuur';
+$lang['use_dataresolve']       = 'Gebruik DATARESOLVE aanroepen voor LIKE declaraties
+';
+$lang['rownumbers']            = 'Schakel "rijnummers" in als verstekwaaarde voor alle tabellen';
