62dc55d6deeae776933ecf56a5de5b5937193b4d
Robert Bogenschneider <bogi@uea.org>
2013-11-17 01:20:49 +0100

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['edit_content_only']     = 'Modifi nur la enhavon, ne la strukturon de datumo';
+$lang['use_dataresolve']       = 'Uzi DATARESOLVE por LIKE-frazoj';
+$lang['rownumbers']            = 'Montri lininombrojn standarde en ĉiuj tabeloj';
