365ef377040e6d42dbbf62be19949d97d09e1864
Schplurtz le Déboulonné <schplurtz@laposte.net>
2014-01-20 12:35:16 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
+ */
+$lang['edit_content_only']     = 'N\'éditer que le contenu des entrées, pas la structure.';
+$lang['use_dataresolve']       = 'Utiliser des appels DATARESOLVE pour les expressions SQL LIKE';
+$lang['rownumbers']            = 'Activer les numéros de lignes (rownumbers) par défaut dans toutes les tables.';
