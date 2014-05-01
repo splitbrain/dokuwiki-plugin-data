a1b368760a36b56708a78c72de110ea9ba9c4c65
Ikuo Obataya <i.obataya@gmail.com>
2014-05-01 23:14:03 +0900

    Update so that filters can recognize number values
    Values in conditions for filter have been treated as string. This commit enables recognition of numbers to cast as real type.

--- syntax/table.php
+++ syntax/table.php
@@ -583,7 +583,7 @@ class syntax_plugin_data_table extends DokuWiki_Syntax_Plugin {
                                   " '".$filter['value']."'"; //value is already escaped
                       }else{
                         $where2 .= ' '.$filter['logic'].' '.$table.'.value '.$filter['compare'].
-                                  " cast(".$filter['value']." as real)"; //value is already escaped
+                                  " cast(".$filter['value']." as real)"; //value is cast as real type
                       }
                     }
                 }
