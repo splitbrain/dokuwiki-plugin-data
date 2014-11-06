d4f71e4c9409a14923cee468c63b482faf830c78
Gerrit Uitslag <klapinklapin@gmail.com>
2014-11-06 01:04:22 +0100

    rename to aliastextbox

--- helper/aliastextbox.php
+++ helper/aliastextbox.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Class helper_plugin_bureaucracy_field_dataplugin
+ * Class helper_plugin_bureaucracy_aliastextbox
  *
  * Create a field with properties defined by given type alias
  * Mostly this is a single line input field, but for enum type a select field.
  */
-class helper_plugin_bureaucracy_field_dataplugin extends helper_plugin_bureaucracy_field {
+class helper_plugin_bureaucracy_aliastextbox extends helper_plugin_bureaucracy_field {
 
     private $args;
     private $additional;
