049c430d9943d8a858178dd4d1dcdd87b8090d87
Gerrit Uitslag <klapinklapin@gmail.com>
2014-12-10 23:20:16 +0100

    translation update

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -4,8 +4,9 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author mprins <mprins@users.sf.net>
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
  */
 $lang['edit_content_only']     = 'Bewerk alleen de inhoud van een datablok, niet de structuur';
 $lang['use_dataresolve']       = 'Gebruik DATARESOLVE aanroepen voor LIKE declaraties
 ';
-$lang['rownumbers']            = 'Schakel "rijnummers" in als verstekwaaarde voor alle tabellen';
+$lang['rownumbers']            = 'Schakel "rijnummers" in als verstekwaarde voor alle tabellen';
