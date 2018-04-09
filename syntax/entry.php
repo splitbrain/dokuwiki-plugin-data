59b7c2b66d4241017379af3d9937a5fc244f670a
Michael Große <grosse@cosmocode.de>
2018-04-09 08:55:37 +0200

    fix: adjust to new startSectionEdit signature
    The signature of startSectionEdit was changed in splitbrain/dokuwiki#2220
    in a non backwards compatible way. This adjusts for that change in a way
    that should still work for pre-Greebo versions.

--- syntax/entry.php
+++ syntax/entry.php
@@ -155,7 +155,13 @@ class syntax_plugin_data_entry extends DokuWiki_Syntax_Plugin {
         global $ID;
         $ret = '';
 
-        $data['classes'] .= ' ' . $R->startSectionEdit($data['pos'], 'plugin_data');
+        $sectionEditData = ['target' => 'plugin_data'];
+        if (!defined('SEC_EDIT_PATTERN')) {
+            // backwards-compatibility for Frusterick Manners (2017-02-19)
+            $sectionEditData = 'plugin_data';
+        }
+        $data['classes'] .= ' ' . $R->startSectionEdit($data['pos'], $sectionEditData);
+
         $ret .= '<div class="inline dataplugin_entry ' . $data['classes'] . '"><dl>';
         $class_names = array();
         foreach($data['data'] as $key => $val) {
