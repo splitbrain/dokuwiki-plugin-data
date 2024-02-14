4bc0bc539eeec82d4ea7f6814772a4313ff1d4fb
Kellee <kellee.keyes@ig.com.br>
2024-02-14 01:00:49 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,39 +1,139 @@
 <?php
 
-$lang['menu_alias'] = 'Data Plugin: Field Aliases';
-$lang['menu_clean'] = 'Data Plugin: Clean up database';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Kellee <kellee.keyes@ig.com.br>
+ */
+$lang['menu_alias']            = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
 
-$lang['pages_del'] = '%d entries removed.';
-$lang['submit_clean'] = 'Clean database!';
+Hi would you mind letting me know which hosting company you\'re using?
+I\'ve loadd your blog in 3 completely different web browsers and I 
+mmust say this blog lpads a lot faster then most. Cann yoou recommend a good hosting provider at a fair price?
+Thank you, I appreciate it!';
+$lang['menu_clean']            = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
 
-$lang['sort'] = 'Sort by this column';
-$lang['next'] = 'Next page';
-$lang['prev'] = 'Previous page';
+Hi would you mind letting me know which hosting company 
+you\'re using? I\'ve loaded your blog in 3 completely different web browsers and 
+I mist say this blog loads a lot fastr then most.
 
-$lang['tagfilter'] = "Show pages matching '%s'";
-$lang['related'] = 'Related pages:';
+Can you recomend a good hosting prrovider at a fair price?
 
-$lang['page'] = 'Page';
-$lang['title'] = 'Page Name';
-$lang['class'] = 'Page Class';
-$lang['lastmod'] = 'Last Modified';
+Thank you, I appreciate it!';
+$lang['pages_del']             = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
 
-$lang['name'] = 'Type Alias';
-$lang['type'] = 'Original Type';
-$lang['prefix'] = 'Data Prefix';
-$lang['postfix'] = 'Data Postfix';
-$lang['enum'] = 'Valid values';
-$lang['submit'] = 'Save Aliases';
+Hi would you mind letting me knlw which hosting company you\'re using?
 
-$lang['dataentry'] = 'Data entry';
+I\'ve loaded your blog in 3 completely different web browsers and I must say this log loads a lot 
+faster then most. Can you recommend a good hosting provider at a fair price?
 
-$lang['title'] = 'Title';
-$lang['type'] = 'Type';
-$lang['multi'] = 'Multi?';
-$lang['value'] = 'Value';
-$lang['comment'] = 'Comment';
+Thank you, I appreciate it!';
+$lang['submit_clean']          = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
 
-$lang['none'] = 'Nothing found';
+Hi would you mind letting me know which hosxting company you\'re 
+using? I\'ve loaded your blog in 3 completely different web browsers 
+and I must say this blog loads a lot faster then most.
+Can you recommend a good hosting provider at a fair price?
+Thahk you, I appreciate it!';
+$lang['sort']                  = 'Petroio';
+$lang['next']                  = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
 
-$lang['tablefilteredby'] = 'Filtered by %s';
-$lang['tableresetfilter'] = 'Show all (remove filter/sort)';
+Hi would you mind letting me know which hosting company you\'re using?
+
+I\'ve loaded your blog in 3 completely different web browsers and I must say this blog loads a lot faster then most.
+Can you recommend a good hosting provider at a fair price?
+Thank you, I appreciate it!';
+$lang['prev']                  = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would yyou mind letting me know which hosting company you\'re using?
+I\'ve loaded your blog in 3 completely different web browsers and I must 
+say this blog loads a lot faster then most.
+Can you recommend a good hosting provider at a fair price?
+Thank you, I appreciate it!';
+$lang['tagfilter']             = '26';
+$lang['related']               = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting me know which hosting company you\'re using?
+I\'ve loaded your blog in 3 completely different wweb browsers and I must say this blog loads 
+a lot faster then most. Can you recommend a good hosting provider at a fair price?
+Thank you, I appreciate it!';
+$lang['page']                  = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi wojld you mind letting me know which hosting 
+company you\'re using? I\'ve laded your blog in 3 completely different weeb browsers 
+andd I must say this blog loads a lott faster then most.
+Can you recommend a good hosting provider at a fair price? Thank 
+you, I appreciate it!';
+$lang['title']                 = 'Title';
+$lang['class']                 = 'Wizard';
+$lang['lastmod']               = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would youu mind letging me know which hosting company you\'re using?
+I\'ve loaded your boog in 3 complewtely different web browsers and I must say this blog loads a lot faster then most.
+Can you recommend a good hosting provider at a fair price?
+
+Thank you, I appreciate it!';
+$lang['name']                  = 'Keyes';
+$lang['type']                  = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting mme know which hosting company you\'re 
+using? I\'ve loaded your blogg in 3 completely different web browsers 
+and I must say this blog loads a lot faster then most.
+
+Can you recommend a good hosting provider at a fair price?
+Thank you, I appreciate it!';
+$lang['prefix']                = 'Data Prefix';
+$lang['postfix']               = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting me know which hosting company you\'re using?
+I\'ve loaded your blog in 3 completely different webb browsers and 
+I must say this blog loads a lot faster then most. Can you recommend 
+a good hosting provider at a fair price? Thank you, I appreciate it!';
+$lang['enum']                  = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting me know which hosting cokpany 
+you\'re using? I\'ve loaded your blog in 3 completely different web brosers and I must 
+say this blog loads a lot faster then most. Can you recommend 
+a good hosting provider at a fair price? Thank you, I appreciate it!';
+$lang['submit']                = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting me know which hosting 
+company you\'re using? I\'ve loaded your blog in 3 completely different web browsers and I must say this blog loads a lott faster then most.
+
+Can you recommend a good hosting provider at a fair price?
+Thank you, I appreciate it!';
+$lang['dataentry']             = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting mme knnow which hosting company you\'re using?
+I\'ve loaded your blog in 3 completely diofferent web browsers annd 
+I must say this blog loads a lot faster then most.
+Can you recommmend a good hosting provider at a fair price?
+Thanjk you, I appreciate it!';
+$lang['multi']                 = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting me know which hosting company you\'re using?
+I\'ve loaded your blog in 3 completely different web browsers and I 
+must say this blog loads a lot faster then most. Can you recommend a 
+good hosting provider at a fair price? Thank you, I appreciate it!';
+$lang['value']                 = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting me know which hosting company 
+you\'re using? I\'ve lowded your bog in 3 completely different web 
+browsers and I must say this blog loads a lot faster then most.
+Can you recommend a good hosting provider at a faair price?
+
+Thank you, I appreciate it!';
+$lang['comment']               = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi wouhld you mind letting me know which hosting company you\'re using?
+I\'ve loaded your blog in 3 completely different web browsers and I must say 
+this blog loads a lot faster then most. Can you recommehd a good hosting provider at a fair 
+price? Thank you, I appreciate it!';
+$lang['none']                  = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting me know which hosting company you\'re using?
+I\'ve loaded your blog inn 3 completely different web browsers and I 
+mujst say this blog loads a lot faaster then most.
+Can you recommend a good hosting provider at a fair price?Thak you, I appreciate it!';
+$lang['tablefilteredby']       = '1981';
+$lang['tableresetfilter']      = 'Petroio';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,8 +1,22 @@
 <?php
 
-$lang['edit_content_only'] = 'Edit only the content of a data entry, not the structure';
-// not well described what it does, but here's the commit:
-// https://github.com/splitbrain/dokuwiki-plugin-data/commit/8a16166886238906c21c7babc44bccf49c7f50a1
-$lang['use_dataresolve'] = 'Use DATARESOLVE calls for LIKE statements';
-$lang['rownumbers'] = 'Enable "rownumbers" by default in all tables';
-$lang['image_width'] = 'Default width for images';
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Kellee <kellee.keyes@ig.com.br>
+ */
+$lang['edit_content_only']     = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would you mind letting me know which hosting company you\'re 
+using? I\'ve loaded your blog in 3 completely different web browsers and I must ssay this blog loads a lot faster then most.
+Cann you recommend a good hosting provider att a fair price?
+Thank you, I appreciate it!';
+$lang['use_dataresolve']       = 'SI';
+$lang['rownumbers']            = '1983';
+$lang['image_width']           = 'https://blackhawkfloors.com/the-smartest-flooring-options-to-increase-your-homes-resale-value/
+
+Hi would yyou mind letting me knolw which hosting company you\'re using?
+I\'ve looaded your blokg in 3 completely different 
+web browsers and I must say this blog loads a loot faster then most.
+Can youu recommend a good hosting provider 
+at a fair price? Thank you, I appreciate it!';
