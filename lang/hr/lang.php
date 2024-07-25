7ca660e1ce5539fddac7dd04585e992b9bd801d5
Dannielle <danniellemcfarland@gmail.com>
2024-07-25 14:35:09 +0200

    translation update

--- lang/hr/lang.php
+++ lang/hr/lang.php
@@ -3,31 +3,91 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Dannielle <danniellemcfarland@gmail.com>
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
-$lang['menu_alias'] = 'Podatkovni dodatak: Pseudonimi polja';
-$lang['menu_clean'] = 'Podatkovni dodatak: Čišćenje baze';
-$lang['pages_del'] = '%d unosa uklonjeno.';
-$lang['submit_clean'] = 'Očisti bazu!';
-$lang['sort'] = 'Poredaj po ovoj koloni';
-$lang['next'] = 'Slijedeća strana';
-$lang['prev'] = 'Prethodna strana';
-$lang['tagfilter'] = 'Prikaži stranice koje odgovaraju \'%s\'';
-$lang['related'] = 'Vezane stranice:';
-$lang['page'] = 'Strana';
-$lang['title'] = 'Naslov';
-$lang['class'] = 'Klasa stranice';
-$lang['lastmod'] = 'Zadnje mijenjano';
-$lang['name'] = 'Pseudonim tipa';
-$lang['type'] = 'Tip';
-$lang['prefix'] = 'Prefiks podatka';
-$lang['postfix'] = 'Postfix podatka';
-$lang['enum'] = 'Ispravne vrijednosti';
-$lang['submit'] = 'Snimi pseudonime';
-$lang['dataentry'] = 'Unos podatka';
-$lang['multi'] = 'Multi?';
-$lang['value'] = 'Vrijednost';
-$lang['comment'] = 'Komentar';
-$lang['none'] = 'Nije nađeno';
-$lang['tablefilteredby'] = 'Filtrirano po %s';
-$lang['tableresetfilter'] = 'Prikaži sve (makni filtriranje i sortiranje)';
+$lang['menu_alias']            = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a 
+litte more on this subject? I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['menu_clean']            = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['pages_del']             = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['submit_clean']          = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+
+I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['sort']                  = 'Combe Moor';
+$lang['next']                  = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+I\'d be very thankful if you could elaborate a little bit 
+further. Bless you!';
+$lang['prev']                  = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write 
+a litte more on this subject? I\'d be very thankful if you could elaborate a little 
+bit further. Bless you!';
+$lang['tagfilter']             = '12';
+$lang['related']               = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte 
+more on this subject? I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['page']                  = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write 
+a litte more on this subject? I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['title']                 = 'Naslov';
+$lang['class']                 = 'Enchanter';
+$lang['lastmod']               = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['name']                  = 'McFarland';
+$lang['type']                  = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+I\'d be very thankful if you could elaborate a little bit further.
+
+Bless you!';
+$lang['prefix']                = 'Prefiks podatka';
+$lang['postfix']               = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['enum']                  = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+
+I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['submit']                = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+
+I\'d be very thankful if you could elaborate a little bit 
+further. Bless you!';
+$lang['dataentry']             = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+I\'d be very thankful if you could elaborate a little bit further.
+
+Bless you!';
+$lang['multi']                 = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could 
+write a litte more on this subject? I\'d be very thankful if you could elaborate a little bit 
+further. Bless you!';
+$lang['value']                 = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more 
+on this subject? I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['comment']               = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you 
+could write a litte more on this subject? I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['none']                  = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+I\'d be very thankful if you could elaborate a little bit further.
+Bless you!';
+$lang['tablefilteredby']       = '1990';
+$lang['tableresetfilter']      = 'Combe Moor';
--- lang/hr/settings.php
+++ lang/hr/settings.php
@@ -3,9 +3,16 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Dannielle <danniellemcfarland@gmail.com>
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
-$lang['edit_content_only'] = 'Uredi samo sadržaj unosa, ne i strukturu';
-$lang['use_dataresolve'] = 'Koristi DATARESOLVE pozive za LIKE izraze';
-$lang['rownumbers'] = 'Omogući brojeve retka kao podrazumijevane u svim tablicama';
-$lang['image_width'] = 'Podrazumijevana širina za slike';
+$lang['edit_content_only']     = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more 
+on this subject? I\'d be very thankful if you could elaborate a 
+little bit further. Bless you!';
+$lang['use_dataresolve']       = 'NA';
+$lang['rownumbers']            = '1980';
+$lang['image_width']           = 'https://socialmphl.com/story19446929/ingredients-of-sumatra-slim-belly-tonic-and-their-benefitsml
+Superb post however I was wondering if you could write a litte more on this subject?
+I\'d be very thankful if you could elaborate a little bit 
+further. Bless you!';
