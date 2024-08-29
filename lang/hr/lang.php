5e84033117e9f385dd5ccfbd07efc225ad4f026f
Minnie <minnieneal@cox.net>
2024-08-29 22:00:49 +0200

    translation update

--- lang/hr/lang.php
+++ lang/hr/lang.php
@@ -3,31 +3,121 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Minnie <minnieneal@cox.net>
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
-$lang['menu_alias'] = 'Podatkovni dodatak: Pseudonimi polja';
-$lang['menu_clean'] = 'Podatkovni dodatak: Čišćenje baze';
-$lang['pages_del'] = '%d unosa uklonjeno.';
-$lang['submit_clean'] = 'Očisti bazu!';
-$lang['sort'] = 'Poredaj po ovoj koloni';
-$lang['next'] = 'Slijedeća strana';
-$lang['prev'] = 'Prethodna strana';
-$lang['tagfilter'] = 'Prikaži stranice koje odgovaraju \'%s\'';
-$lang['related'] = 'Vezane stranice:';
-$lang['page'] = 'Strana';
-$lang['title'] = 'Naslov';
-$lang['class'] = 'Klasa stranice';
-$lang['lastmod'] = 'Zadnje mijenjano';
-$lang['name'] = 'Pseudonim tipa';
-$lang['type'] = 'Tip';
-$lang['prefix'] = 'Prefiks podatka';
-$lang['postfix'] = 'Postfix podatka';
-$lang['enum'] = 'Ispravne vrijednosti';
-$lang['submit'] = 'Snimi pseudonime';
-$lang['dataentry'] = 'Unos podatka';
-$lang['multi'] = 'Multi?';
-$lang['value'] = 'Vrijednost';
-$lang['comment'] = 'Komentar';
-$lang['none'] = 'Nije nađeno';
-$lang['tablefilteredby'] = 'Filtrirano po %s';
-$lang['tableresetfilter'] = 'Prikaži sve (makni filtriranje i sortiranje)';
+$lang['menu_alias']            = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about my blog not working correctly in Explorer but looks 
+great in Firefox. Do you have any recommendations to help fix 
+this issue?';
+$lang['menu_clean']            = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site. Do you ever 
+run into any internet browser compatibility issues?
+A small number of my blog visitors have complained 
+about my blog not working correctly in Explorer but looks great in Firefox.
+Do you have any recommendations to help fix this issue?';
+$lang['pages_del']             = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site. Do you ever run into 
+any internet browser compatibility issues? A small 
+number of my blog visitors have complained about my blog not working correctly 
+in Explorer but looks great in Firefox. Do you have any recommendations to help fix this 
+issue?';
+$lang['submit_clean']          = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+
+A small number of my blog visitors have complained about 
+my blog not working correctly in Explorer but looks great in Firefox.
+Do you have any recommendations to help fix this issue?';
+$lang['sort']                  = 'Minden Hahlen';
+$lang['next']                  = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site. Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about my blog not working correctly in Explorer but looks great in Firefox.
+Do you have any recommendations to help fix this issue?';
+$lang['prev']                  = 'https://dcshop.biz
+I\'m really loving the theme/design of your web 
+site. Do you ever run into any internet browser compatibility 
+issues? A small number of my blog visitors have complained about my blog not working 
+correctly in Explorer but looks great in Firefox. Do you have any recommendations to help fix this issue?';
+$lang['tagfilter']             = '13';
+$lang['related']               = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site. Do you ever run into any internet browser compatibility issues?
+
+A small number of my blog visitors have complained about my blog not working 
+correctly in Explorer but looks great in Firefox.
+
+Do you have any recommendations to help fix this issue?';
+$lang['page']                  = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+
+Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about my blog not working correctly in Explorer 
+but looks great in Firefox. Do you have any recommendations to 
+help fix this issue?';
+$lang['title']                 = 'Naslov';
+$lang['class']                 = 'Cleric';
+$lang['lastmod']               = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about my blog not working correctly 
+in Explorer but looks great in Firefox. Do 
+you have any recommendations to help fix this issue?';
+$lang['name']                  = 'Neal';
+$lang['type']                  = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about my blog not 
+working correctly in Explorer but looks great in Firefox.
+Do you have any recommendations to help fix this 
+issue?';
+$lang['prefix']                = 'Prefiks podatka';
+$lang['postfix']               = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+
+A small number of my blog visitors have complained about my blog not working correctly in Explorer but looks great in Firefox.
+Do you have any recommendations to help fix this issue?';
+$lang['enum']                  = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained 
+about my blog not working correctly in Explorer but looks great in Firefox.
+Do you have any recommendations to help fix this issue?';
+$lang['submit']                = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site. Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about my blog not working correctly in Explorer but looks 
+great in Firefox. Do you have any recommendations to help fix this issue?';
+$lang['dataentry']             = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+
+A small number of my blog visitors have complained about my blog not working correctly in Explorer but looks great in Firefox.
+
+Do you have any recommendations to help fix this issue?';
+$lang['multi']                 = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site. Do you ever run into 
+any internet browser compatibility issues? A small number of my 
+blog visitors have complained about my blog not working 
+correctly in Explorer but looks great in Firefox. Do you have any recommendations to help fix this 
+issue?';
+$lang['value']                 = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility 
+issues? A small number of my blog visitors have complained 
+about my blog not working correctly in Explorer but looks great in Firefox.
+Do you have any recommendations to help fix this issue?';
+$lang['comment']               = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about my blog not working 
+correctly in Explorer but looks great in Firefox. Do you have any recommendations to help fix this issue?';
+$lang['none']                  = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site. 
+Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about my blog not working correctly in Explorer 
+but looks great in Firefox. Do you have any recommendations 
+to help fix this issue?';
+$lang['tablefilteredby']       = '1982';
+$lang['tableresetfilter']      = 'Minden Hahlen';
--- lang/hr/settings.php
+++ lang/hr/settings.php
@@ -3,9 +3,20 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Minnie <minnieneal@cox.net>
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
-$lang['edit_content_only'] = 'Uredi samo sadržaj unosa, ne i strukturu';
-$lang['use_dataresolve'] = 'Koristi DATARESOLVE pozive za LIKE izraze';
-$lang['rownumbers'] = 'Omogući brojeve retka kao podrazumijevane u svim tablicama';
-$lang['image_width'] = 'Podrazumijevana širina za slike';
+$lang['edit_content_only']     = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site. Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about my blog 
+not working correctly in Explorer but looks great in Firefox.
+Do you have any recommendations to help fix this issue?';
+$lang['use_dataresolve']       = 'NW';
+$lang['rownumbers']            = '1983';
+$lang['image_width']           = 'https://dcshop.biz
+I\'m really loving the theme/design of your web site.
+Do you ever run into any internet browser compatibility issues?
+A small number of my blog visitors have complained about 
+my blog not working correctly in Explorer but looks great in Firefox.
+
+Do you have any recommendations to help fix this issue?';
