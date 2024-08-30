c04a5e5a6fe411849037c22d18c0e35de04152a3
Lavina <lavinacollette@yahoo.com>
2024-08-30 13:00:49 +0200

    translation update

--- lang/hr/lang.php
+++ lang/hr/lang.php
@@ -3,31 +3,89 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Lavina <lavinacollette@yahoo.com>
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
-$lang['menu_alias'] = 'Podatkovni dodatak: Pseudonimi polja';
-$lang['menu_clean'] = 'Podatkovni dodatak: Čišćenje baze';
-$lang['pages_del'] = '%d unosa uklonjeno.';
-$lang['submit_clean'] = 'Očisti bazu!';
-$lang['sort'] = 'Poredaj po ovoj koloni';
-$lang['next'] = 'Slijedeća strana';
-$lang['prev'] = 'Prethodna strana';
-$lang['tagfilter'] = 'Prikaži stranice koje odgovaraju \'%s\'';
-$lang['related'] = 'Vezane stranice:';
-$lang['page'] = 'Strana';
-$lang['title'] = 'Naslov';
-$lang['class'] = 'Klasa stranice';
-$lang['lastmod'] = 'Zadnje mijenjano';
-$lang['name'] = 'Pseudonim tipa';
-$lang['type'] = 'Tip';
-$lang['prefix'] = 'Prefiks podatka';
-$lang['postfix'] = 'Postfix podatka';
-$lang['enum'] = 'Ispravne vrijednosti';
-$lang['submit'] = 'Snimi pseudonime';
-$lang['dataentry'] = 'Unos podatka';
-$lang['multi'] = 'Multi?';
-$lang['value'] = 'Vrijednost';
-$lang['comment'] = 'Komentar';
-$lang['none'] = 'Nije nađeno';
-$lang['tablefilteredby'] = 'Filtrirano po %s';
-$lang['tableresetfilter'] = 'Prikaži sve (makni filtriranje i sortiranje)';
+$lang['menu_alias']            = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest 
+but your sites really nice, keep it up! I\'ll go ahead and bookmark your website to come back in the future.
+
+Cheers';
+$lang['menu_clean']            = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your 
+sites really nice, keep it up! I\'ll go ahead and bookmark 
+your website to come back in the future. Cheers';
+$lang['pages_del']             = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites 
+really nice, keep it up! I\'ll go ahead and bookmark your website 
+to come back in the future. Cheers';
+$lang['submit_clean']          = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites 
+really nice, keep it up! I\'ll go ahead and bookmark your website 
+to come back in the future. Cheers';
+$lang['sort']                  = 'Collalto Sabino';
+$lang['next']                  = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest 
+but your sites really nice, keep it up! I\'ll go ahead and bookmark 
+your website to come back in the future. Cheers';
+$lang['prev']                  = 'https://blackbet.cm/
+I’m not that much of a online reader to 
+be honest but your sites really nice, keep it up! I\'ll go ahead and bookmark your website to come 
+back in the future. Cheers';
+$lang['tagfilter']             = '12';
+$lang['related']               = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your 
+sites really nice, keep it up! I\'ll go ahead and bookmark your website 
+to come back in the future. Cheers';
+$lang['page']                  = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites really nice, keep it up!
+I\'ll go ahead and bookmark your website to come back in the future.
+Cheers';
+$lang['title']                 = 'Naslov';
+$lang['class']                 = 'Gladiator';
+$lang['lastmod']               = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites really nice, keep it up!
+I\'ll go ahead and bookmark your website to come back in the future.
+Cheers';
+$lang['name']                  = 'Collette';
+$lang['type']                  = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites 
+really nice, keep it up! I\'ll go ahead and bookmark your website to 
+come back in the future. Cheers';
+$lang['prefix']                = 'Prefiks podatka';
+$lang['postfix']               = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites really nice, keep it up!
+I\'ll go ahead and bookmark your website to come back in the future.
+Cheers';
+$lang['enum']                  = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your 
+sites really nice, keep it up! I\'ll go ahead and bookmark your website 
+to come back in the future. Cheers';
+$lang['submit']                = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites really nice, keep it up!
+I\'ll go ahead and bookmark your website to come back 
+in the future. Cheers';
+$lang['dataentry']             = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites really 
+nice, keep it up! I\'ll go ahead and bookmark your website to come back in the future.
+Cheers';
+$lang['multi']                 = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but 
+your sites really nice, keep it up! I\'ll go ahead and bookmark 
+your website to come back in the future.
+Cheers';
+$lang['value']                 = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your 
+sites really nice, keep it up! I\'ll go ahead and bookmark your website to come back in the future.
+Cheers';
+$lang['comment']               = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites really nice, keep it up!
+
+I\'ll go ahead and bookmark your website to come back in the future.
+Cheers';
+$lang['none']                  = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites really nice, 
+keep it up! I\'ll go ahead and bookmark your website to come back in the future.
+Cheers';
+$lang['tablefilteredby']       = '1986';
+$lang['tableresetfilter']      = 'Collalto Sabino';
--- lang/hr/settings.php
+++ lang/hr/settings.php
@@ -3,9 +3,17 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Lavina <lavinacollette@yahoo.com>
  * @author Davor Turkalj <turki.bsc@gmail.com>
  */
-$lang['edit_content_only'] = 'Uredi samo sadržaj unosa, ne i strukturu';
-$lang['use_dataresolve'] = 'Koristi DATARESOLVE pozive za LIKE izraze';
-$lang['rownumbers'] = 'Omogući brojeve retka kao podrazumijevane u svim tablicama';
-$lang['image_width'] = 'Podrazumijevana širina za slike';
+$lang['edit_content_only']     = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites really nice, keep it up!
+I\'ll go ahead and bookmark your website to come back in the future.
+
+Cheers';
+$lang['use_dataresolve']       = 'RI';
+$lang['rownumbers']            = '1989';
+$lang['image_width']           = 'https://blackbet.cm/
+I’m not that much of a online reader to be honest but your sites really nice, 
+keep it up! I\'ll go ahead and bookmark your website 
+to come back in the future. Cheers';
